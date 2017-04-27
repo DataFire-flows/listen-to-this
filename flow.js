7640ee0acbca2f34d6153b5e3668863efea8bd18
Travis CI <bobby.brennan@gmail.com>
2017-04-27 18:06:41 +0000

    update for v2

--- flow.js
+++ flow.js
@@ -1,59 +1,41 @@
 const datafire = require('datafire');
-const flow = module.exports = new datafire.Flow("Create a Spotify playlist from r/listenToThis", "Create a new playlist every day using suggestions from Reddit");
-const spotify = datafire.Integration.new('spotify').as('default');
-const reddit = datafire.Integration.new('reddit-rss');
+const spotify = require('@datafire/spotify').actions;
+const reddit = require('@datafire/reddit_rss').actions;
 
-flow.step('spotify_user', {
-  do: spotify.get("/me"),
-  params: (data) => {
-    return {}
-  }
-})
-
-flow.step('reddit', {
-  do: reddit.subreddit(),
-  params: () => {
-    return {subreddit: 'listentothis'}
-  }
-})
-
-flow.step('tracks', {
-  do: spotify.get("/search"),
-  params: (data) => {
-    var tracks = data.reddit.feed.entries.slice(0, 10);
-    return tracks.map(function(entry) {
-        return {
-          type: "track",
-          q: entry.title.replace(/\[.*\]/g, '').replace(/\(.*\)/g, ''),
-        }
-    })
-  }
-})
-
-flow.step('add_playlist', {
-  do: spotify.post("/users/{user_id}/playlists"),
-  params: (data) => {
-    return {
-      user_id: data.spotify_user.id,
-      body: {
-        name: "r/listentothis for " + new Date().toISOString().slice(0,10),
-      }
-    }
-  }
-})
-
-flow.step('add_tracks', {
-  do: spotify.post("/users/{user_id}/playlists/{playlist_id}/tracks"),
-  params: (data) => {
-    return {
-      user_id: data.spotify_user.id,
-      playlist_id: data.add_playlist.id,
-      uris: data.tracks.filter(function(searchResults) {
-        return searchResults.tracks.items.length;
-      }).map(function(searchResults) {
-        return searchResults.tracks.items[0].uri;
+module.exports = new datafire.Action({
+  title: "Create a Spotify playlist from r/listenToThis",
+  description: "Create a new playlist every day using suggestions from Reddit",
+  handler: (input, context) => {
+    return datafire.flow(context)
+      .then(_ => spotify.me.get({}, context))
+      .then(spotifyUser => reddit.subreddit({subreddit: 'listentothis'}))
+      .then(posts => {
+        let entries = posts.feed.entries.slice(0, 10);
+        return Promise.all(entries.map(entry => {
+          return spotify.search.get({
+            type: 'track',
+            q: entry.title.replace(/\[.*\]/g, '').replace(/\(.*\)/g, ''),
+          }, context)
+        }))
       })
-      .join(',')
-    }
+      .then(searchResults => {
+        return spotify.users.user_id.playlists.post({
+          user_id: context.results.spotifyUser.id,
+          body: {
+            name: "r/listentothis for " + new Date().toISOString().slice(0,10),
+          },
+        }, context)
+      })
+      .then(playlist => {
+        return spotify.users.user_id.playlists.playlist_id.tracks.post({
+          user_id: context.results.spotifyUser.id,
+          playlist_id: playlist.id,
+          uris: context.results.searchResults.filter(function(searchResult) {
+              return searchResult.tracks.items.length;
+            }).map(function(searchResult) {
+              return searchResult.tracks.items[0].uri;
+            }).join(','),
+        }, context)
+      })
   }
 })
